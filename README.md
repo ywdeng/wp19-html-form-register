9ece7954e50655cf6283e16d4103808ee7617439
joseph.deng@gmail.com <ntnl3914>
2019-03-12 17:45:42 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Sample Registration Form
+
+A sample [Express](http://expressjs.com/) application to demo HTML registration form.
+
+## Run locally
+
+1. Install [Node.js and npm](https://nodejs.org/)
+1. Run `npm install`
+1. Run `npm start`
+1. Visit [http://localhost:3000](http://localhost:3000)
+
+
---
+++ public/javascripts/validate.js
@@ -0,0 +1,37 @@
+function validatePassword() {
+    var pwdCheck = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    var pwd = document.getElementById('thePassword');
+    if (!pwdCheck.test(pwd.value)) {
+        pwd.setCustomValidity('Password is not good enough');
+        pwd.style.backgroundColor = 'Crimson';
+        return false;
+    }
+    pwd.setCustomValidity('');
+    pwd.style.backgroundColor = 'LightGreen';
+    return true;
+}
+
+function confirmPassword() {
+    var pwd = document.getElementById('thePassword');
+    var pwd2 = document.getElementById('confirmPassword');
+    if (pwd.value != pwd2.value) {
+        pwd2.setCustomValidity("Passwords Don't Match!");
+        pwd2.style.backgroundColor = 'Crimson';
+        return false;
+    }
+    pwd2.setCustomValidity('');
+    pwd2.style.backgroundColor = 'LightGreen';
+    return true;
+}
+
+function validateInput() {
+    if (validatePassword() && confirmPassword()) {
+        return true;
+    }
+    var pwd = document.getElementById('thePassword');
+    var pwd2 = document.getElementById('confirmPassword');
+    pwd2.value = '';
+    pwd.select();
+    pwd.focus();
+    return false;
+}
